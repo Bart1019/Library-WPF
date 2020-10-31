15729ea22c40ef6abb847fdb75edd3037b5d1e93
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:04:43 +0100

    Adding enum for books

---
+++ Library/Library.Data/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Data
+{
+    public enum Enum
+    {
+        Roman,
+        Historic,
+        Adventure,
+        SciFi,
+        Document
+    }
+}
