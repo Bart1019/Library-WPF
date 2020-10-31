f605b47c9169bf2a953b85dd464f66aed5c30135
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:11:08 +0100

    Adding implementation of IUserRepo

--- Library/Library.Data/Interfaces/IUserRepository.cs
+++ Library/Library.Data/Interfaces/IUserRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IUserRepository
     {
+        IQueryable<User> GetAll();
+        User GetById(int id);
+        void Delete(int id);
+        void Edit(User user);
+        void Add(User user);
     }
 }
