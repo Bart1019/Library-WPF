de8be039852b524d1be7a2f0a832eb4b1e344754
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:09:06 +0100

    Adding implementation of IBookInterface

--- Library/Library.Data/Interfaces/IBookRepository.cs
+++ Library/Library.Data/Interfaces/IBookRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IBookRepository
     {
+        IQueryable<Book> GetAll();
+        Book GetById(int id);
+        Book GetByType(BookEnum bookType);
+        void Delete(int id);
+        void Edit(Book book);
+        void Add(Book book);
     }
 }
