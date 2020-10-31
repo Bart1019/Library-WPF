2c516701acc20eb19104b72415a16a2f878b0020
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:07:39 +0100

    Adding implementation of other models classes

--- Library/Library.Data/BookEnum.cs
+++ Library/Library.Data/BookEnum.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Library.Data
 {
-    public enum Enum
+    public enum BookEnum
     {
         Roman,
         Historic,
--- Library/Library.Data/Models/Rental.cs
+++ Library/Library.Data/Models/Rental.cs
@@ -6,5 +6,7 @@ namespace Library.Data.Models
 {
     public class Rental
     {
+        public Dictionary<Book, User> RentedBooks { get; set; }
+        //Todo rethink how this class should work
     }
 }
