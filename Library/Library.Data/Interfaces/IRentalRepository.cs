9095a231821a6af538ac4ca309d86c5a49bf3acd
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:11:20 +0100

    Adding implementation of IRentalRepo

--- Library/Library.Data/Interfaces/IRentalRepository.cs
+++ Library/Library.Data/Interfaces/IRentalRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface IRentalRepository
     {
+        Dictionary<Book, User> GetAllRentals();
+        Dictionary<Book, User> GetRentalById(int id);
     }
 }
