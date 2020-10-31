c6dc022efbb82c7cab3e474f7238811fa4483e8a
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-10-31 16:09:48 +0100

    Adding implementation of ILibraryRepo

--- Library/Library.Data/Interfaces/ILibraryRepository.cs
+++ Library/Library.Data/Interfaces/ILibraryRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Models;
 
 namespace Library.Data.Interfaces
 {
     public interface ILibraryRepository
     {
+        IQueryable<Book> GetAllAvailableBooks();
+        IQueryable<Book> GetAllUnAvailableBooks();s
     }
 }
