3a0f2418e5187d0dfe28b3bc9ebabc1ede7d7fdf
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-01 13:04:10 +0100

    Implementing user service

--- Library/Library.Logic/Services/UserService.cs
+++ Library/Library.Logic/Services/UserService.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Logic.Services
 {
     public class UserService
     {
-        //todo
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return userRepository.GetAllUsers().ToList();
+        }
+
+        public User GetUser(int id)
+        {
+            return userRepository.GetUserById(id);
+        }
+
+        public void DeleteUser(int id)
+        {
+            userRepository.DeleteUser(id);
+        }
+
+        public void EditUser(User user)
+        {
+            userRepository.EditUser(user);
+        }
+
+        public void AddUser(User user)
+        {
+            userRepository.AddUser(user);
+        }
     }
 }
