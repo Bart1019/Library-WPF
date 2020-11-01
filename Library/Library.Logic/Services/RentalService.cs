7f93900a8fe9db9441b03dd804047a230544aab3
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-01 14:28:54 +0100

    Adding initial implementation of rental service

--- Library/Library.Logic/Services/RentalService.cs
+++ Library/Library.Logic/Services/RentalService.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Logic.Services
 {
     public class RentalService
     {
-        //todo
+        private readonly IRentalRepository rentalRepository;
+        private readonly ILibraryRepository libraryRepository;
+        private readonly IUserRepository userRepository;
+
+        public RentalService(IRentalRepository rentalRepository, ILibraryRepository libraryRepository, IUserRepository userRepository)
+        {
+            this.rentalRepository = rentalRepository;
+            this.libraryRepository = libraryRepository;
+            this.userRepository = userRepository;
+        }
+
+        public List<Rental> GetAllRentals()
+        {
+            return rentalRepository.GetAllRentals().ToList();
+        }
+
+        public Rental GetRental(int id)
+        {
+            return rentalRepository.GetRentalById(id);
+        }
     }
 }
