0c0f8406ef1ad7c1aaed317307a35ed31e3a1c27
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-01 13:09:34 +0100

    Implementing library service

--- Library/Library.Logic/Services/LibraryService.cs
+++ Library/Library.Logic/Services/LibraryService.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Logic.Services
 {
     public class LibraryService
     {
-        //todo
+        private readonly ILibraryRepository libraryRepository;
+
+        public LibraryService(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        public List<Book> GetAllAvailableBooks()
+        {
+            return libraryRepository.GetAllAvailableBooks().ToList();
+        }
+
+        public List<Book> GetAllUnavailableBooks()
+        {
+            return libraryRepository.GetAllUnavailableBooks().ToList();
+        }
     }
 }
