c2ad64f5a94ba5f1687d263832ab0b1cc5898e14
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-01 12:46:03 +0100

    Changing rental model

--- Library/Library.Data/Models/Rental.cs
+++ Library/Library.Data/Models/Rental.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Library.Data.Interfaces;
 
 namespace Library.Data.Models
 {
     public class Rental
     {
-        private User user;
-        private OurLibrary library;
-
-        public Rental(User user, OurLibrary library)
-        {
-            this.user = user;
-            this.library = library;
-        }
-
         public int Id { get; set; }
         public DateTime RentalDate { get; set; }
         public DateTime GiveBackDate { get; set; }
-        public List<User> UsersRentalHistory { get; set; }
+        public List<User> RentalUsersHistory { get; set; } = new List<User>();
+        public List<Book> RentedBooksHistory { get; set; } = new List<Book>();
     }
 }
