c21c3fa1f55e25474ef70202f3d22adc7dd4ea26
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-11-10 15:16:57 +0100

    Minor fixes

---
+++ Library/Library.Tests/UnitTests/RepositoriesTests/BooksStateRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Library.Data;
+using Library.Data.Models;
+using Library.Logic.Repositories;
+using Xunit;
+using Book = Library.Data.Models.BooksCatalog.Book;
+
+namespace Library.Tests.UnitTests.RepositoriesTests
+{
+    public class BooksStateRepositoryTests
+    {
+        private readonly MockBooksStateRepository _booksStateRepository;
+
+        public BooksStateRepositoryTests()
+        {
+            _booksStateRepository = new MockBooksStateRepository();
+        }
+
+        [Fact]
+        public void ShouldGetAllAvailableBooks()
+        {
+            //Arrange
+
+            //Act
+            var resultedBooks = _booksStateRepository.GetAllAvailableBooks();
+
+            //Assert
+            Assert.True(resultedBooks.Count.Equals(6));
+
+        }
+
+        [Fact]
+        public void ShouldUpdateBooksAmount()
+        {
+            //Arrange
+
+            //Act
+            _booksStateRepository.UpdateBooksAmount(1, 33);
+
+            //Assert
+        }
+    }
+}
