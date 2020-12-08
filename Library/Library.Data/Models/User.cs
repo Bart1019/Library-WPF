0fd680f9e0e10a6c405f50afd01aa885740186e2
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-12-08 20:54:19 +0100

    Adding view model for user list and changing user model

--- Library/Library.Data/Models/User.cs
+++ Library/Library.Data/Models/User.cs
@@ -1,13 +1,68 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Data
 {
-    public class User
+    public class User : INotifyPropertyChanged
     {
+        private string _name;
+        private string _surname;
+        private int _amountOfBooksRented;
+
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int AmountOfBooksRented { get; set; }
+        public string Name 
+        { 
+            get => _name;
+
+            set 
+            {
+                if (_name == value) return;
+                _name = value; 
+                RaisePropertyChanged("Name");
+                RaisePropertyChanged("FullName");
+            } 
+        }
+        public string Surname
+        {
+            get => _surname;
+
+            set
+            {
+                if (_surname == value) return;
+                _surname = value;
+                RaisePropertyChanged("Surname");
+                RaisePropertyChanged("FullName");
+            }
+        }
+
+        public int AmountOfBooksRented
+        {
+            get => _amountOfBooksRented;
+
+            set
+            {
+                if (_amountOfBooksRented == value) return;
+                _amountOfBooksRented = value;
+                RaisePropertyChanged("AmountOfBooksRented");
+            }
+        }
+
+
+
+        public string FullName
+        {
+            get
+            {
+                return _name + " " + _surname;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
---
+++ Library/Library.Logic/ViewModels/UserListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Library.Data;
+
+namespace Library.Logic
+{
+    public class UserListViewModel
+    {
+        public ObservableCollection<User> Users { get; set; }
+    }
+}
